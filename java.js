bdd7880d75bfe3fc8c7d911094aab782ce1bf24b
M.Ramautar <144722341+MarisaR12@users.noreply.github.com>
2024-03-12 18:02:44 +0000

    Initial commit

---
+++ java.js
@@ -0,0 +1,452 @@
+function a() {
+   document.getElementById("a").style.background = "#FF2222";
+  
+}
+
+    function b() {
+      document.getElementById("b").style.background = "#56FF45";
+    }
+
+function c() {
+   document.getElementById("c").style.background = "#FF2222";
+
+}
+
+function d() {
+   document.getElementById("d").style.background = "#FF2222";
+
+}
+
+function a1() {
+   document.getElementById("a1").style.background = "#FF2222";
+
+}
+
+    function b1() {
+      document.getElementById("b1").style.background = "#56FF45";
+    }
+
+function c1() {
+   document.getElementById("c1").style.background = "#FF2222";
+
+}
+
+function d1() {
+   document.getElementById("d1").style.background = "#FF2222";
+
+}
+
+
+
+
+
+
+function a2() {
+   document.getElementById("a2").style.background = "#FF2222";
+
+}
+
+    function b2() {
+      document.getElementById("b2").style.background = "#FF2222";
+    }
+
+function c2() {
+   document.getElementById("c2").style.background = "#56FF45";
+
+}
+
+function d2() {
+   document.getElementById("d2").style.background = "#56FF45";
+
+}
+
+
+
+
+function a3() {
+   document.getElementById("a3").style.background = "#FF2222";
+
+}
+
+    function b3() {
+      document.getElementById("b3").style.background = "#56FF45";
+    }
+
+function c3() {
+   document.getElementById("c3").style.background = "#FF2222";
+
+}
+
+function d3() {
+   document.getElementById("d3").style.background = "#FF2222";
+
+}
+
+
+
+function a4() {
+   document.getElementById("a4").style.background = "#56FF45";
+
+}
+
+    function b4() {
+      document.getElementById("b4").style.background = "#FF2222";
+    }
+
+function c4() {
+   document.getElementById("c4").style.background = "#FF2222";
+
+}
+
+function d4() {
+   document.getElementById("d4").style.background = "#FF2222";
+
+}
+
+
+function a5() {
+   document.getElementById("a5").style.background = "#FF2222";
+
+}
+
+    function b5() {
+      document.getElementById("b5").style.background = "#FF2222";
+    }
+
+function c5() {
+   document.getElementById("c5").style.background = "#56FF45";
+
+}
+
+function d5() {
+   document.getElementById("d5").style.background = "#FF2222";
+
+}
+
+
+
+function a6() {
+   document.getElementById("a6").style.background = "#56FF45";//red
+
+}
+
+    function b6() {
+      document.getElementById("b6").style.background = "#FF2222";
+    }
+
+function c6() {
+   document.getElementById("c6").style.background = "#FF2222";//green
+
+}
+
+function d6() {
+   document.getElementById("d6").style.background = "#FF2222";
+
+}
+function e6() {
+   document.getElementById("e6").style.background = "#56FF45";
+
+}
+
+
+function a7() {
+   document.getElementById("a7").style.background = "#FF2222";//red
+
+}
+
+    function b7() {
+      document.getElementById("b7").style.background = "#56FF45";
+    }
+
+function c7() {
+   document.getElementById("c7").style.background = "#56FF45";//green
+
+}
+
+function d7() {
+   document.getElementById("d7").style.background = "#FF2222";
+
+}
+
+function a8 () {
+   document.getElementById("a8").style.background = "#FF2222";//red
+
+}
+
+    function b8() {
+      document.getElementById("b8").style.background = "#FF2222";
+    }
+
+function c8() {
+   document.getElementById("c8").style.background = "#FF2222";//green
+
+}
+
+function d8() {
+   document.getElementById("d8").style.background = "#56FF45";
+
+}
+
+
+function a9 () {
+   document.getElementById("a9").style.background = "#FF2222";//red
+
+}
+
+    function b9() {
+      document.getElementById("b9").style.background = "#56FF45";
+    }
+
+function c9() {
+   document.getElementById("c9").style.background = "#FF2222";//green
+
+}
+
+function d9() {
+   document.getElementById("d9").style.background = "#56FF45";
+
+}
+
+function a10 () {
+   document.getElementById("a10").style.background = "#56FF45";//red
+
+}
+
+    function b10() {
+      document.getElementById("b10").style.background = "#FF2222";
+    }
+
+function c10() {
+   document.getElementById("c10").style.background = "#FF2222";//green
+
+}
+
+function a11 () {
+   document.getElementById("a11").style.background = "#FF2222";//red
+
+}
+
+    function b11() {
+      document.getElementById("b11").style.background = "#56FF45";
+    }
+
+function c11() {
+   document.getElementById("c11").style.background = "#FF2222";//green
+}
+
+function a12 () {
+   document.getElementById("a12").style.background = "#FF2222";//red
+
+}
+
+    function b12() {
+      document.getElementById("b12").style.background = "#FF2222";
+    }
+
+function c12() {
+   document.getElementById("c12").style.background = "#56FF45";//green
+}
+//13
+function a13 () {
+   document.getElementById("a13").style.background = "#FF2222";//red
+
+}
+
+    function b13() {
+      document.getElementById("b13").style.background = "#56FF45"
+    }
+
+function c13() {
+   document.getElementById("c13").style.background = "#FF2222";//green
+}
+function d13() {
+   document.getElementById("d13").style.background = "#FF2222";//green
+}
+//question14
+function a14 () {
+   document.getElementById("a14").style.background = "#FF2222";//red
+
+}
+
+    function b14() {
+      document.getElementById("b14").style.background = "#FF2222"
+    }
+
+function c14() {
+   document.getElementById("c14").style.background = "#56FF45";//green
+}
+function d14() {
+   document.getElementById("d14").style.background = "#FF2222";//green
+}
+//question 15
+function a15 () {
+   document.getElementById("a15").style.background = "#FF2222";//red
+
+}
+
+    function b15() {
+      document.getElementById("b15").style.background = "#FF2222"
+    }
+
+function c15() {
+   document.getElementById("c15").style.background = "#FF2222";//green
+}
+function d15() {
+   document.getElementById("d15").style.background = "#56FF45";//green
+}
+//question 16
+function a16 () {
+   document.getElementById("a16").style.background = "#56FF45";//red
+
+
+}
+    function b16() {
+      document.getElementById("b16").style.background = "#FF2222"
+    }
+
+function c16() {
+   document.getElementById("c16").style.background = "#FF2222";//green
+}
+function d16() {
+   document.getElementById("d16").style.background = "#FF2222";//green
+}
+function e16(){
+  document.getElementById("e16").style.background= "#FF2222";
+}
+//question17
+function a17 () {
+   document.getElementById("a17").style.background = "#FF2222";//red
+
+
+}
+    function b17() {
+      document.getElementById("b17").style.background = "#56FF45"
+    }
+
+function c17() {
+   document.getElementById("c17").style.background = "#FF2222";//green
+}
+function d17() {
+   document.getElementById("d17").style.background = "#FF2222";//green
+}
+function e17(){
+  document.getElementById("e17").style.background= "#FF2222";
+}
+//question18
+function a18 () {
+   document.getElementById("a18").style.background = "#FF2222";//red
+
+
+}
+    function b18() {
+      document.getElementById("b18").style.background = "#FF2222"
+    }
+
+function c18() {
+   document.getElementById("c18").style.background = "#56FF45";//green
+}
+function d18() {
+   document.getElementById("d18").style.background = "#FF2222";//green
+}
+function e18(){
+  document.getElementById("e18").style.background= "#FF2222";
+}
+//question19
+function a19 () {
+   document.getElementById("a19").style.background = "#FF2222";//red
+
+
+}
+    function b19() {
+      document.getElementById("b19").style.background = "#FF2222"
+    }
+
+function c19() {
+   document.getElementById("c19").style.background = "#FF2222";//green
+}
+function d19() {
+   document.getElementById("d19").style.background = "#56FF45";//green
+}
+function e19(){
+  document.getElementById("e19").style.background= "#FF2222";
+}
+//question20
+function a20 () {
+   document.getElementById("a20").style.background = "#FF2222";//red
+
+
+}
+    function b20() {
+      document.getElementById("b20").style.background = "#FF2222"
+    }
+
+function c20() {
+   document.getElementById("c20").style.background = "#FF2222";//green
+}
+function d20() {
+   document.getElementById("d20").style.background = "#FF2222";//green
+}
+function e20(){
+  document.getElementById("e20").style.background= "#56FF45";
+}
+
+//question21
+function a21 () {
+   document.getElementById("a21").style.background = "#FF2222";//red
+
+
+}
+    function b21() {
+      document.getElementById("b21").style.background = "#56FF45"
+    }
+
+function c21() {
+   document.getElementById("c21").style.background = "#FF2222";//green
+}
+function d21() {
+   document.getElementById("d21").style.background = "#FF2222";//green
+}
+//question22
+function a22 () {
+   document.getElementById("a22").style.background = "#56FF45";//red
+
+
+}
+    function b22() {
+      document.getElementById("b22").style.background = "#FF2222"
+    }
+
+function c22() {
+   document.getElementById("c22").style.background = "#FF2222";//green
+}
+function d22() {
+   document.getElementById("d22").style.background = "#FF2222";//green
+}
+//question23
+function a23 () {
+   document.getElementById("a23").style.background = "#56FF45";//red
+
+
+}
+    function b23() {
+      document.getElementById("b23").style.background = "#FF2222"
+    }
+
+function c23() {
+   document.getElementById("c23").style.background = "#FF2222";//green
+}
+function d23() {
+   document.getElementById("d23").style.background = "#FF2222";//green
+}
+
+
+
+
+
+
+
+
+
+
+
+
